1bfda1e612b4df82f29f391d497ca7d95e56abda
Magomed Makhmudov <mmriadovich@gmail.com>
2018-07-12 18:59:49 +0300

    feedforward

--- feedfoward/FeedForwardNN.py
+++ feedfoward/FeedForwardNN.py
@@ -1,51 +1,24 @@
 import numpy as np
+import random
+
+class FeedFowrard(object):
 
 
-class FeedForwardNN(object):
+    def __init__(self, sizes = [2,3,2], output=True):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y,1) for y in sizes[:1]]
+        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
 
-    def __init__(self, X, y, counteHiddenLayer, countHiddenNodes, output_nodes = 1):
-
-        '''init weight'''
-        self.input_layer = np.random.rand(countHiddenNodes, len(X))
-        self.hidden_layer = np.array([np.random.rand(countHiddenNodes, countHiddenNodes) for i in range(counteHiddenLayer)])
-        self.output_layer = np.random.rand(output_nodes, countHiddenNodes) # wrong
-
-        self.X = np.array(X)
-        self.y = np.array(y, ndmin=2).T
+        self.output = output
 
 
-    def _sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def train(self):
-        forward_result = self.forward()
+    def feedforward(self, a):
+        pass
 
 
-    def forward(self):
-        hidden_layer_input = np.dot(self.input_layer, self.X)
-        flag = True
-        for indx, wl in enumerate(self.hidden_layer):
-            if flag:
-                input_for_layer = hidden_layer_input
-            input_for_layer = np.dot(wl, input_for_layer)
-            output_layer = self._sigmoid(input_for_layer)
-        return self._sigmoid(np.dot(self.output_layer, output_layer))
 
 
-def main():
-    X = np.array([
-        [0,0],
-        [1,0],
-        [0,1],
-        [1,1]
-    ])
-    y = np.array([0, 0, 0, 1], ndmin=2).T
-
-    # print(X)
-    # print(y)
-
-    f = FeedForwardNN(X, y, 1, 2)
-    print(f.forward())
 
 if __name__ == '__main__':
-    main()
+    nn = FeedFowrard()
